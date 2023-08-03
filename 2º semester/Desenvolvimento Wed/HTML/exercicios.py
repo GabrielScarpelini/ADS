717e468b3b73a5a7d34c85bbcf9472f36b2e8545
GabrielScarpelini <gabriel.spavia@gmail.com>
2023-08-03 16:19:30 -0300

    2º semetre

---
+++ 2º semester/Desenvolvimento Wed/HTML/exercicios.py
@@ -0,0 +1,12 @@
+class Triangulo:
+    def __init__(self, lado_a, lado_b, lado_c):
+        self.lado_a = lado_a
+        self.lado_b = lado_b
+        self.lado_c = lado_c 
+    
+    def perimetro(a, b, c):
+        print(a+b+c)
+
+triangulo_retangulo = Triangulo(5, 6, 9)
+
+print(triangulo_retangulo)
