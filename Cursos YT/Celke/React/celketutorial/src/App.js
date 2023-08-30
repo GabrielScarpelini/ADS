89eb36c18e9b89ef9fa574c3e33d93c80a2a20d0
GabrielScarpelini <gabriel.spavia@gmail.com>
2023-08-30 12:28:45 -0300

    estudos sobre React

---
+++ Cursos YT/Celke/React/celketutorial/src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { Button } from 'reactstrap';
+import Headers from "./Headers";
+import Article from "./Artilcle";
+import Footer from "./Footer";
+
+function App (props){
+  return(
+    <div>
+      <Headers />
+      <Article />
+      <Footer />
+      <Button color="danger">Danger!</Button>
+    </div>
+  )
+};
+
+export default App;
